50c8e8fe9a4fc9735242982d2406b5db4cbe1a3e
Jheelam <jheelamhossain147@gmail.com>
2025-01-23 11:02:22 +0530

    changes made

--- src/HollowHourGlass.java
+++ src/HollowHourGlass.java
@@ -1,11 +1,36 @@
 import java.util.Scanner;
 
 public class HollowHourGlass {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        for(int i=0;i<n;i++){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
 
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * (N - i) - 1; j++) {
+                if (j == 0 || j == 2 * (N - i) - 2) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = N - 2; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * (N - i) - 1; j++) {
+                if (j == 0 || j == 2 * (N - i) - 2) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
         }
     }
 }
