46e1b24dc623b0c1c7cb869ce2f5ac5df7d972dc
Jheelam <jheelamhossain147@gmail.com>
2025-01-28 14:34:04 +0530

    changes made

---
+++ src/WordsLengthMap.java
@@ -0,0 +1,18 @@
+import java.util.TreeMap;
+
+public class WordsLengthMap {
+    public static void main(String[] args) {
+        // Create a TreeMap to store words as keys and their lengths as values
+        TreeMap<String, Integer> wordsMap = new TreeMap<>();
+
+        // Add words to the map
+        wordsMap.put("apple", "apple".length());
+        wordsMap.put("banana", "banana".length());
+        wordsMap.put("cherry", "cherry".length());
+        wordsMap.put("date", "date".length());
+        wordsMap.put("elderberry", "elderberry".length());
+
+        // Print the TreeMap to observe the sorted order of keys
+        System.out.println("TreeMap of words and their lengths: " + wordsMap);
+    }
+}
